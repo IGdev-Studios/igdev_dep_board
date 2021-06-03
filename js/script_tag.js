3e89d0b804a411202e0c2c205ef6bc80d7a94a3b
keke38000 <75201282+keke38000@users.noreply.github.com>
2021-06-03 13:55:35 +0200

    Possibility to select number of arrows to display

--- js/script_tag.js
+++ js/script_tag.js
@@ -103,9 +103,13 @@ function checkSaved(){
     }
     localStorage.setItem('LinesAndStopTAG',JSON.stringify(LinesAndStop));
    }
+   if(localStorage.getItem('numberRows') == null){
+       let numberRows = 4
+       localStorage.setItem('numberRows',numberRows);
+   }
 }
 
-function editLinesAndStop(stop,lines){
+function editLinesAndStop(stop,lines,numberRows){
     
  
         var i = 0;
@@ -124,6 +128,7 @@ function editLinesAndStop(stop,lines){
             "lines":lines
         }
         localStorage.setItem('LinesAndStopTAG',JSON.stringify(LinesAndStop));
+        localStorage.setItem('numberRows',numberRows);
         loading();
         refresh();
     

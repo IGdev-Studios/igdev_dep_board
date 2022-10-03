aa28e061d6bf0a341b78306ff6dd9546f5365bf0
keke38000 <kelyanselme38@gmail.com>
2022-10-03 18:26:00 +0200

    bug fix + bases fichier unique dep board

--- js/script_tag.js
+++ js/script_tag.js
@@ -1,52 +1,58 @@
-async function getSchedules(){
+async function getNetworkName(){
+    const urlParams = new URLSearchParams(window.location.search);
+			const networkSelected = urlParams.get('network');
+            return networkSelected;
+}
+
+async function getSchedules() {
     let LinesAndStop = JSON.parse(localStorage.getItem('LinesAndStopTAG'));
-    try{
-        let response =  await fetchApi("https://data.mobilites-m.fr/api/routers/default/index/clusters/"+ LinesAndStop.zone +"/stoptimes");
+    try {
+        let response = await fetchApi("https://data.mobilites-m.fr/api/routers/default/index/clusters/" + LinesAndStop.zone + "/stoptimes");
         const timesMontfleury = await response.json();
 
-    var finalSchedules = [];
-    timesMontfleury.forEach(element => {
-        var id = element.pattern.id.split(":");
-        if(!LinesAndStop.lines){
-            element.times.forEach(time => {
-                finalSchedules.push({
-                    "lastStop":element.pattern.lastStopName,
-                    "time":time.realtimeArrival,
-                    "realtime":time.realtime,
-                    "line":id[1]
+        var finalSchedules = [];
+        timesMontfleury.forEach(element => {
+            var id = element.pattern.id.split(":");
+            if (!LinesAndStop.lines) {
+                element.times.forEach(time => {
+                    finalSchedules.push({
+                        "lastStop": element.pattern.lastStopName,
+                        "time": time.realtimeArrival,
+                        "realtime": time.realtime,
+                        "line": id[1]
+                    });
                 });
-            });
-        }else{
-        if(LinesAndStop.lines.includes(id[1])){
-            element.times.forEach(time => {
-                finalSchedules.push({
-                    "lastStop":element.pattern.lastStopName,
-                    "time":time.realtimeArrival,
-                    "realtime":time.realtime,
-                    "line":id[1]
-                });
-            });
+            } else {
+                if (LinesAndStop.lines.includes(id[1])) {
+                    element.times.forEach(time => {
+                        finalSchedules.push({
+                            "lastStop": element.pattern.lastStopName,
+                            "time": time.realtimeArrival,
+                            "realtime": time.realtime,
+                            "line": id[1]
+                        });
+                    });
+                }
+            }
+        });
+        if (!finalSchedules.length) {
+            return "Aucun passage de prévu";
+        } else {
+            return finalSchedules;
         }
-    }
-    });
-    if (!finalSchedules.length){
-        return "Aucun passage de prévu";
-    }else{
-        return finalSchedules;
-    }
-   
-    }catch{
+
+    } catch {
         return "Erreur chargement";
     }
 
 };
 
-async function fetchApi(url){
+async function fetchApi(url) {
     const controller = new AbortController()
     const timeoutId = setTimeout(() => controller.abort(), 5000)
-    const response = await fetch(url,{
-        headers:{
-            "origin":"webBrowser"
+    const response = await fetch(url, {
+        headers: {
+            "origin": "webBrowser"
         },
         signal: controller.signal
     });
@@ -54,99 +60,116 @@ async function fetchApi(url){
     return response
 }
 
-function secToDelay(time){
+function secToDelay(time) {
     let currentDate = new Date();
-    let secondes = ((currentDate.getHours())*3600)+((currentDate.getMinutes())*60)+currentDate.getSeconds();
-    let difference = Math.floor((time-secondes)/60);
-        return difference;
-    
+    let secondes = ((currentDate.getHours()) * 3600) + ((currentDate.getMinutes()) * 60) + currentDate.getSeconds();
+    let difference = Math.floor((time - secondes) / 60);
+    return difference;
+
 }
 
-function sort(tab){
+function sort(tab) {
     var values = []
-    for (var key in tab){
-        if(tab[key].time >= 0){
+    for (var key in tab) {
+        if (tab[key].time >= 0) {
             values.push([key, tab[key].time, tab[key].lastStop, tab[key].realtime, tab[key].line])
         }
     }
 
-    values.sort(function compare(i,j){
-        return i[1]-j[1];
+    values.sort(function compare(i, j) {
+        return i[1] - j[1];
     });
     var finalSchedules = []
     values.forEach(element => {
-        if(secToDelay(element[1]) >= 0){
-        finalSchedules.push(
-            {
-                "lastStop":element[2],
-                "time":secToDelay(element[1]),
-                "realtime":element[3],
-                "line":element[4]
-            }
-        );
+        if (secToDelay(element[1]) >= 0) {
+            finalSchedules.push(
+                {
+                    "lastStop": element[2],
+                    "time": secToDelay(element[1]),
+                    "realtime": element[3],
+                    "line": element[4]
+                }
+            );
         }
     });
     return finalSchedules;
 }
 
-function checkTime(i){
-    if (i<10){
-        i = "0"+i;
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
     }
     return i;
 }
 
-function getTime(){
+function getTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     h = checkTime(h);
     m = checkTime(m);
-    return h+":"+m;
+    return h + ":" + m;
 }
 
-function checkSaved(){
-   if(localStorage.getItem('LinesAndStopTAG') == null){
-    let LinesAndStop = {
-        "arret":"Montfleury",
-        "zone":"SEM:GENMONTFLEU",
-        "lines":["16"]
-    }
-    localStorage.setItem('LinesAndStopTAG',JSON.stringify(LinesAndStop));
-   }
-   if(localStorage.getItem('numberRows') == null){
-       let numberRows = 4
-       localStorage.setItem('numberRows',numberRows);
-   }
-}
-
-function editLinesAndStop(stop,lines,numberRows){
-    
- 
-        var i = 0;
-        var found = 0;
-        for (key in dictStops){
-            if(dictStops[key][1] == stop){
-                found = i;
-            }
-            i++;
-        }
-        var linesSplit = lines.split(",");
-        console.log(linesSplit);
+function checkSaved() {
+    if (localStorage.getItem('LinesAndStopTAG') == null) {
         let LinesAndStop = {
-            "arret":stop,
-            "zone":"SEM:"+dictStops[found][0],
-            "lines":lines
+            "arret": "Montfleury",
+            "zone": "SEM:GENMONTFLEU",
+            "lines": ["16"]
         }
-        localStorage.setItem('LinesAndStopTAG',JSON.stringify(LinesAndStop));
-        localStorage.setItem('numberRows',numberRows);
-        loading();
-        refresh();
-    
+        localStorage.setItem('LinesAndStopTAG', JSON.stringify(LinesAndStop));
+    }
+    if (localStorage.getItem('numberRows') == null) {
+        let numberRows = 4
+        localStorage.setItem('numberRows', numberRows);
+    }
 }
 
-function minutesToHours(minutes){
+function editLinesAndStop(stop, lines, numberRows) {
+
+
+    var i = 0;
+    var found = 0;
+    for (key in dictStops) {
+        if (dictStops[key][1] == stop) {
+            found = i;
+        }
+        i++;
+    }
+    var linesSplit = lines.split(",");
+    console.log(linesSplit);
+    let LinesAndStop = {
+        "arret": stop,
+        "zone": "SEM:" + dictStops[found][0],
+        "lines": lines
+    }
+    localStorage.setItem('LinesAndStopTAG', JSON.stringify(LinesAndStop));
+    localStorage.setItem('numberRows', numberRows);
+    loading();
+    refresh();
+
+}
+
+function minutesToHours(minutes) {
     var currentDate = new Date();
-    var newDate = new Date(currentDate.getTime() + minutes*60000 +60000);
+    var newDate = new Date(currentDate.getTime() + minutes * 60000 + 60000);
     return checkTime(newDate.getHours()) + ":" + checkTime(newDate.getMinutes())
+}
+
+
+async function loadStopAndLines() {
+    let mainText = ""
+    const reponse = await fetch('js/stopsTag.txt').then((data) => {
+        data.text().then((text) => {
+            mainText = text;
+        }).catch((error) => {
+            console.log(error);
+            mainText = "error";
+        });
+    }).catch((error) => {
+        console.log(error);
+        mainText = "error";
+    });
+    return mainText;
 }

62ad79aa01dadc5877cbbeea96cad545fed32e6a
keke38000 <kelyanselme38@gmail.com>
2022-10-05 20:11:17 +0200

    bug fix la liste des arrêts pouvait ne pas charger

--- js/script_tag.js
+++ js/script_tag.js
@@ -159,17 +159,7 @@ function minutesToHours(minutes) {
 
 
 async function loadStopAndLines() {
-    let mainText = ""
-    const reponse = await fetch('js/stopsTag.txt').then((data) => {
-        data.text().then((text) => {
-            mainText = text;
-        }).catch((error) => {
-            console.log(error);
-            mainText = "error";
-        });
-    }).catch((error) => {
-        console.log(error);
-        mainText = "error";
-    });
-    return mainText;
+    const response = await fetch('js/stopsTag.txt')
+    const data = await response.text();
+    return data;
 }

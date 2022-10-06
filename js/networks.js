8d1692e90d02ecf11caddb6f272785c5fa631994
keke38000 <kelyanselme38@gmail.com>
2022-10-06 11:26:43 +0200

    fin transition vers fichier depart unique
    attention l'afficheur tpv ne fonctionnne pas (metromobilité a changé les codes d'arrêts un bug fix va venir)

--- js/networks.js
+++ js/networks.js
@@ -1,6 +1,11 @@
-const networks = { tag: { code: "SEM:", slogan: "Je monte, Je valide !", image: "https://upload.wikimedia.org/wikipedia/fr/thumb/a/a0/TAG_%28Logotype_2014%29.svg/512px-TAG_%28Logotype_2014%29.svg.png", rightIcon : "img/Cita_60px.png", rightIconText: "V1" } }
+const networks = {
+    tag: { code: "SEM:", slogan: "Je monte, Je valide !", image: "https://upload.wikimedia.org/wikipedia/fr/thumb/a/a0/TAG_%28Logotype_2014%29.svg/512px-TAG_%28Logotype_2014%29.svg.png", rightIcon: "img/Cita_60px.png", rightIconText: "V1" },
+    tpv: { code: "TPV:", slogan: "Prenez de l'avance", image: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4a/Logo_Pays_Voironnais_Mobilit%C3%A9.svg/1024px-Logo_Pays_Voironnais_Mobilit%C3%A9.svg.png", rightIcon: "img/IGdev.png", rightIconText: "V1" },
+    tougo: { code: "GSV:", slogan: "Prenez de l'avance", image: "https://upload.wikimedia.org/wikipedia/fr/1/19/Logo_R%C3%A9seau_TouGo.jpg", rightIcon: "img/IGdev.png", rightIconText: "V1" },
+}
 
-function getNetwork(name){
+
+function getNetwork(name) {
     return networks[name];
 }
 
--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-async function getNetworkName(){
+async function getNetworkName() {
     const urlParams = new URLSearchParams(window.location.search);
-			const networkSelected = urlParams.get('network');
-            return networkSelected;
+    const networkSelected = urlParams.get('network');
+    return networkSelected;
 }
 
 async function getSchedules() {
-    let LinesAndStop = JSON.parse(localStorage.getItem('LinesAndStopTAG'));
+    let networkName = await getNetworkName();
+    console.log(networkName);
+    let itemToLoad = "";
+    switch (networkName) {
+        case 'tag':
+            itemToLoad = "LinesAndStopTAG";
+            break;
+        case 'tpv':
+            itemToLoad = "LinesAndStopTPV";
+            break;
+        case 'tougo':
+            itemToLoad = "LinesAndStopGSV";
+            break;
+        default:
+            itemToLoad = "LinesAndStopTAG";
+            break;
+    }
+    let LinesAndStop = JSON.parse(localStorage.getItem(itemToLoad));
     try {
         let response = await fetchApi("https://data.mobilites-m.fr/api/routers/default/index/clusters/" + LinesAndStop.zone + "/stoptimes");
+        console.log("https://data.mobilites-m.fr/api/routers/default/index/clusters/" + LinesAndStop.zone + "/stoptimes");
         const timesMontfleury = await response.json();
 
         var finalSchedules = [];
@@ -111,14 +129,50 @@ function getTime() {
     return h + ":" + m;
 }
 
-function checkSaved() {
-    if (localStorage.getItem('LinesAndStopTAG') == null) {
-        let LinesAndStop = {
-            "arret": "Montfleury",
-            "zone": "SEM:GENMONTFLEU",
-            "lines": ["16"]
-        }
-        localStorage.setItem('LinesAndStopTAG', JSON.stringify(LinesAndStop));
+async function checkSaved() {
+    let networkName = await getNetworkName();
+    switch (networkName) {
+        case 'tag':
+            if (localStorage.getItem('LinesAndStopTAG') == null) {
+                let LinesAndStop = {
+                    "arret": "Montfleury",
+                    "zone": "SEM:GENMONTFLEU",
+                    "lines": ["16"]
+                }
+                localStorage.setItem('LinesAndStopTAG', JSON.stringify(LinesAndStop));
+            }
+            break;
+        case 'tpv':
+            if (localStorage.getItem('LinesAndStopTPV') == null) {
+                let LinesAndStop = {
+                    "arret": "Voiron, LEPRINCE RINGUET",
+                    "zone": "TPV:ST00408",
+                    "lines": ["1"]
+                }
+                localStorage.setItem('LinesAndStopTPV', JSON.stringify(LinesAndStop));
+            }
+
+            break;
+        case 'tougo':
+            if (localStorage.getItem('LinesAndStopGSV') == null) {
+                let LinesAndStop = {
+                    "arret": "Meylan, NORBERT SEGARD",
+                    "zone": "GSV:GEN20088",
+                    "lines": ["G2"]
+                }
+                localStorage.setItem('LinesAndStopGSV', JSON.stringify(LinesAndStop));
+            }
+            break;
+        default:
+            if (localStorage.getItem('LinesAndStopTAG') == null) {
+                let LinesAndStop = {
+                    "arret": "Montfleury",
+                    "zone": "SEM:GENMONTFLEU",
+                    "lines": ["16"]
+                }
+                localStorage.setItem('LinesAndStopTAG', JSON.stringify(LinesAndStop));
+            }
+            break;
     }
     if (localStorage.getItem('numberRows') == null) {
         let numberRows = 4
@@ -126,8 +180,7 @@ function checkSaved() {
     }
 }
 
-function editLinesAndStop(stop, lines, numberRows) {
-
+async function editLinesAndStop(stop, lines, numberRows) {
 
     var i = 0;
     var found = 0;
@@ -138,13 +191,43 @@ function editLinesAndStop(stop, lines, numberRows) {
         i++;
     }
     var linesSplit = lines.split(",");
-    console.log(linesSplit);
+    let networkName = await getNetworkName();
+    let code = "";
+    switch (networkName) {
+        case 'tag':
+            code = "SEM:"
+            break;
+        case 'tpv':
+            code = "TPV:"
+            break;
+        case 'tougo':
+            code = "GSV:"
+            break;
+        default:
+            code = "SEM:"
+            break;
+    }
+    let itemToSave = "";
+    switch (networkName) {
+        case 'tag':
+            itemToSave = "LinesAndStopTAG";
+            break;
+        case 'tpv':
+            itemToSave = "LinesAndStopTPV";
+            break;
+        case 'tougo':
+            itemToSave = "LinesAndStopGSV";
+            break;
+        default:
+            itemToSave = "LinesAndStopTAG";
+            break;
+    }
     let LinesAndStop = {
         "arret": stop,
-        "zone": "SEM:" + dictStops[found][0],
+        "zone": code + dictStops[found][0],
         "lines": lines
     }
-    localStorage.setItem('LinesAndStopTAG', JSON.stringify(LinesAndStop));
+    localStorage.setItem(itemToSave, JSON.stringify(LinesAndStop));
     localStorage.setItem('numberRows', numberRows);
     loading();
     refresh();
@@ -158,8 +241,23 @@ function minutesToHours(minutes) {
 }
 
 
-async function loadStopAndLines() {
-    const response = await fetch('js/stopsTag.txt')
+async function loadStopAndLines(networkName) {
+    let fileName = "";
+    switch (networkName) {
+        case 'tag':
+            fileName = "js/stopsTag.txt";
+            break;
+        case 'tpv':
+            fileName = "js/stopsTpv.txt";
+            break;
+        case 'tougo':
+            fileName = "js/stopsTougo.txt";
+            break;
+        default:
+            fileName = "js/stopsTag.txt";
+            break;
+    }
+    const response = await fetch(fileName);
     const data = await response.text();
     return data;
 }

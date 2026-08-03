af982ff5b8b4b0184b89272f5766a6e67ba07f6e
Kelyan Pegeot Selme <kelyan.pegeot-selme@eviden.com>
2026-08-03 09:30:06 +0200

    feat: home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: Home
+    }
+];
